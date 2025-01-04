5b988d1662a1d7d17b15d2b47c14fcfc3b94dd6c
BrenoLima54 <breno54lima54@gmail.com>
2025-01-04 19:00:02 -0300

    fix: fix image bug

--- src/components/CardAtleta.jsx
+++ src/components/CardAtleta.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CardAtleta = ({ atleta }) => {
   return (
     <div className="card">
-      <img src={atleta.image} alt={atleta.name} />
+      <img src={atleta.photo} alt={atleta.name} />
       <h2>{atleta.name}</h2>
       <h3>{atleta.nickname}</h3>
       <p>{atleta.weight}</p>
