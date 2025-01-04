a115db82bc1fd42b9813d0eca5833435eedab935
BrenoLima54 <breno54lima54@gmail.com>
2025-01-04 18:07:24 -0300

    feat: create CardAtleta

---
+++ src/components/CardAtleta.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CardAtleta = ({ atleta }) => {
+  return (
+    <div className="card">
+      <img src={atleta.image} alt={atleta.name} />
+      <h2>{atleta.name}</h2>
+      <h3>{atleta.nickname}</h3>
+      <p>{atleta.weight}</p>
+      <p>{atleta.categoty}</p>
+    </div>
+  );
+};
+
+export default CardAtleta;
