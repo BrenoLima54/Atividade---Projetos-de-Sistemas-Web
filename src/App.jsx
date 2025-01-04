a3a6a508a99c3fb6517e2b7b938cc92947daa4db
BrenoLima54 <breno54lima54@gmail.com>
2025-01-03 22:07:39 -0300

    feat: add api-experiment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://v1.mma.api-sports.io/fighters?id=691", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "v1.mma.api-sports.io",
+        "x-rapidapi-key": import.meta.env.VITE_API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.response[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return <></>;
 }
 
