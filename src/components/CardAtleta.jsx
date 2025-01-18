1ace842b7b40a88e93bd23ff8740420c4dd7d19d
Nath <playerxmatt@gmail.com>
2025-01-18 17:03:23 -0300

    fix: image not loading

--- src/components/CardAtleta.jsx
+++ src/components/CardAtleta.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Heart } from "lucide-react";
-import anderson from "../assets/anderson.png";
+import imageNotFound from "../assets/image_not_found.png";
 
 const CardAtleta = ({ atleta, addToFavorites }) => {
   return (
     <div className="card">
-      {/* <img src={atleta.image || 'default-image.jpg'} alt={atleta.name} /> */}
-      <img src={anderson}/>
+      <img
+        src={atleta.photo}
+        alt={atleta.name}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src = imageNotFound;
+        }}
+      />
       <h2>{atleta.name}</h2>
       <h3>{atleta.nickname}</h3>
       <p>{atleta.weight}</p>
       <p>{atleta.category}</p>
 
       <button className="fav-butto" onClick={() => addToFavorites(atleta)}>
-         <Heart/> 
-         </button>
+        <Heart />
+      </button>
     </div>
   );
 };
