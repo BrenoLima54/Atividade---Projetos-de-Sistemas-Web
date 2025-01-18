5eca0aa15c627da78cd366d7ee594d8ed69df9a8
Nath <playerxmatt@gmail.com>
2025-01-18 18:08:18 -0300

    chore: remove API key from services

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'https://v1.mma.api-sports.io/fighters?id=';
-const API_KEY = '2165239fd0477aa6447448dcd51a3aa5';
-
+const dotenv = import.meta.env;
 export async function fetchAthletes(search) {
   try {
-    const response = await axios.get(`https://v1.mma.api-sports.io/fighters?search=${search}`, {
-      headers: {
-        'x-apisports-key': API_KEY
+    const response = await axios.get(
+      `${dotenv.VITE_API_URL}/fighters?search=${search}`,
+      {
+        headers: { "x-rapidapi-key": dotenv.VITE_API_KEY },
       }
-    });
+    );
     console.log(response.data);
     return response.data.response;
   } catch (error) {
-    console.error('Erro na busca de atletas:', error);
+    console.error("Erro na busca de atletas:", error);
     return [];
   }
 }
-
